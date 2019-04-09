c33f8b962b85a551e1cd56b573d7d41268d27597
Max Schridde <maxjschridde@gmail.com>
2019-04-08 18:07:11 -0700

    fix GET_MANY syntax

--- build/index.js
+++ build/index.js
@@ -126,8 +126,13 @@ exports.default = function (apiUrl) {
 
       case _actions.GET_MANY:
         {
+          // const query = {
+          //   filter: JSON.stringify({ id: params.ids }),
+          // };
           var _query = {
-            filter: JSON.stringify({ id: params.ids })
+            filter: {
+              id: params.ids
+            }
           };
           url = apiUrl + '/' + resource + '?' + (0, _qs.stringify)(_query);
           break;
